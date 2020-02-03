9106cee6d570ae022f93b04e7c3f1058c8562550
Shanjinur Islam <spondoncsebuet@gmail.com>
2020-02-04 01:03:58 +0600

    logo added

--- lib/view/home_screen.dart
+++ lib/view/home_screen.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:hotel_management/model/service.dart';
 import 'package:hotel_management/view/service_card.dart';
 
-
 class HomeScreen extends StatelessWidget {
   List<Service> row1 = new List<Service>();
   List<Service> row2 = new List<Service>();
@@ -12,7 +11,8 @@ class HomeScreen extends StatelessWidget {
     Service rs1 = new Service("images/bar.png", "Bar", "lorem", false);
     Service rs2 = new Service("images/bulter.png", "Butler", "Details", true);
     Service rs3 = new Service("images/gym.png", "Gym", "Details", false);
-    Service rs4 = new Service("images/massage.png", "Massage", "Details", false);
+    Service rs4 =
+        new Service("images/massage.png", "Massage", "Details", false);
 
     row1.add(rs1);
     row1.add(rs2);
@@ -22,7 +22,8 @@ class HomeScreen extends StatelessWidget {
     Service r2s1 = new Service("images/yoga.png", "Yoga", "Details", true);
     Service r2s2 = new Service("images/spa.png", "Spa", "Details", false);
     Service r2s3 = new Service("images/swim.png", "Swimming", "Details", false);
-    Service r2s4 = new Service("images/room_service.png", "Room Service", "Details", false);
+    Service r2s4 = new Service(
+        "images/room_service.png", "Room Service", "Details", false);
 
     row2.add(r2s1);
     row2.add(r2s2);
@@ -37,9 +38,14 @@ class HomeScreen extends StatelessWidget {
         body: Stack(
           children: <Widget>[
             Positioned(
-              child: Text(
-                'Radisson Blu',
-                style: TextStyle(fontSize: 25, fontWeight: FontWeight.w900),
+              child: Row(
+                children: <Widget>[
+                  Text(
+                    'Radisson Blu\t',
+                    style: TextStyle(fontSize: 25, fontWeight: FontWeight.w900),
+                  ),
+                  Image.asset("images/logo.jpg",scale: 15,)
+                ],
               ),
               top: 45,
               left: 40,
